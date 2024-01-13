f5fc1df3603e493ed030d1499d84798d6b263192
babotavo419 <one.faith419@gmail.com>
2024-01-13 15:43:08 -0500

    added some copy

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -7,28 +7,52 @@ const LandingPage = () => {
     
     <div className="main-container">
       <Header />
-      <h1 className="welcome-header">Welcome to the Landing Page!</h1>
+      <h1 className="welcome-header">Illuminate Your World with FaceIt Custom Neon Signs</h1>
       <section className="section1">
-        <h2 >Section 1 Title</h2>
-        <p>This is the content for Section 1.</p>
-       
+        <h2 >Welcome to FaceIt, where your brightest ideas shine in neon!</h2>
+        <p>
+          At FaceIt, we're passionate about lighting up your world with custom,
+          high-quality neon signs. With over two decades of expertise in the field, 
+          our commitment is to turn your vision into a luminous reality. Perfect for 
+          businesses, special events, or personal home decor, our neon signs are crafted 
+          to add a unique glow to any space.
+        </p>
       </section>
 
       <section className="section2">
-        <h2>Section 2 Title</h2>
-        <p>This is the content for Section 2.</p>
+        <h2>Why Choose FaceIt?</h2>
+        <p>
+          -Tailored to Your Taste: We specialize in creating custom designs that reflect 
+            your style and needs.
+          - Unmatched Quality: Our signs are made with top-tier materials, ensuring
+            both beauty and longevity.
+          - Eco-Friendly Efficiency: FaceIt neon signs are not just eye-catching; they're
+            also energy-efficient.
+          - Skilled Craftsmanship: Our team's expertise in neon artistry guarantees that each 
+            sign is a piece of illuminated perfection.
+          - Prompt Delivery: We value your time, ensuring swift and reliable delivery of your 
+            neon sign.
+        </p>
         
       </section>
 
       <section className="section3">
-        <h2>Section 3 Title</h2>
-        <p>This is the content for Section 3.</p>
+        <h2>Our Services</h2>
+        <p>
+            - Business Signage: Boost your brand's visibility with a striking neon sign.
+            - Home Decor: Personalize your space with a unique neon touch.
+            - Event Signage: Make your events memorable with our custom neon signs.
+            - Bespoke Projects: No matter your idea, we're here to bring it to neon life.
+        </p>
        
       </section>
 
       <section className="section4">
-        <h2>Section 4 Title</h2>
-        <p>This is the content for Section 4.</p>
+        <h2>Ready to Light Up Your Idea?</h2>
+        <p>
+        Get in touch with us today to begin crafting your dream neon sign. At FaceIt, 
+        we're excited to make your bright ideas a neon reality!
+        </p>
         
       </section>
 
