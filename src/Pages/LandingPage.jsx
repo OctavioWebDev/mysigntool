855ccb36b780f1d42130810da61e341b550f9fc0
babotavo419 <one.faith419@gmail.com>
2024-02-01 18:35:42 -0500

    added ico

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -3,13 +3,15 @@ import { Link } from 'react-router-dom';
 import promoVideo from '../assets/Videos/Scotty B\'s LED\'s Promo.mp4';
 import uncleJohns from '../assets/ScottyBsLEDpics/245398213_1300694530390420_8635895171047700084_n.jpeg';
 import digiSign1 from '../assets/ScottyBsLEDpics/digisign1design.png'
+import { ReactComponent as SBLEDSLogo } from '../assets/Logos/SBLEDSLogo.svg';
+
 const LandingPage = () => {
 
   return (
     <div className="flex flex-col min-h-screen">
       <header className="px-4 lg:px-6 h-14 flex items-center">
         <Link className="flex items-center justify-center" href="#">
-          <MountainIcon className="h-6 w-6" />
+          <SBLEDSLogo className="h-7 w-7" />
           <span className="sr-only">Scotty B's LED's</span>
         </Link>
         <nav className="ml-auto flex gap-4 sm:gap-6">
@@ -310,25 +312,6 @@ const LandingPage = () => {
         </nav>
       </footer>
     </div>
-  )
-}
-
-function MountainIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="m8 3 4 8 5-5 5 15H2L8 3z" />
-    </svg>
   )
 }
 
